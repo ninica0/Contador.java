d7849ca94fafaea0b6c6bf9daaabe1df9f6bd81f
ninica0 <santosnico048@gmail.com>
2024-09-30 14:36:16 -0300

    feat: Contador de um número X até outro número

---
+++ Contador.java
@@ -0,0 +1,14 @@
+public class Contador {
+
+	public static void main(String[] args) {
+//		Declaração de contador
+		int i;
+//		Iniciando o looping com valores de 0 até 10
+		for(i=0; i<100; i++) {
+//			Printar o contador
+			System.out.println(i);
+			
+
+	}
+	}
+}
